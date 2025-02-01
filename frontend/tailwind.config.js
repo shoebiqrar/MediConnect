ca4a7dbc2e87ebf34974e0ebea405e90c408151d
Shoeb Iqrar <shoebiqrar6@gmail.com>
2025-02-02 00:10:41 +0530

    latest commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,3 +17,12 @@ export default {
   plugins: [],
 }
 
+
+/*gridTemplateColumns: { 'auto': 'repeat(auto-fill, minmax(200px, 1fr))' }
+Customizes grid behavior in Tailwind CSS.
+auto: Creates a responsive grid with flexible columns.
+repeat(auto-fill, minmax(200px, 1fr)):
+Automatically fills the row with as many columns as possible.
+Each column is at least 200px wide but can grow to take up remaining space equally (1fr).
+Result: A flexible, responsive grid that adjusts based on the container's width.
+Let me know if you need fur */
